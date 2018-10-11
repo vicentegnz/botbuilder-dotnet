536cce23248e7cbd26b505ef25ff50be2b620377
Drew Marsh <drmarsh@microsoft.com>
2018-10-11 14:05:28 -0700

    Initial work on dialogs specific integration APIs
     * Adds `DialogBot` class that provides a boiler plate implementation
    of an `IBot` that works with a `DialogSet` to drive a conversation.
     * Adds overloads for standard `AddBot` API that allows other types of
    bot registrations (factory and singleton)
     * Adds `AddDialogBot` API to register a `DialogBot` which builds on
    teh new `AddBot` APIs by registering a singleton `IBot` instance.

---
+++ libraries/Microsoft.Bot.Builder.Dialogs/DialogBot.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Dialogs
+{
+    public class DialogBot<TDialogSet> : IBot
+        where TDialogSet : DialogSet
+    {
+        public static readonly string DefaultRootDialogId = "root";
+
+        private readonly TDialogSet _dialogSet;
+        private readonly string _rootDialogId;
+
+        public DialogBot(IStatePropertyAccessor<DialogState> dialogStatePropertyAccessor) : this(dialogStatePropertyAccessor, DefaultRootDialogId)
+        {
+        }
+
+        public DialogBot(IStatePropertyAccessor<DialogState> dialogStatePropertyAccessor, string rootDialogId)
+        {
+            if (dialogStatePropertyAccessor == null)
+            {
+                throw new ArgumentNullException(nameof(dialogStatePropertyAccessor));
+            }
+
+            if (string.IsNullOrEmpty(rootDialogId))
+            {
+                throw new ArgumentNullException(nameof(rootDialogId));
+            }
+
+            _dialogSet = Activator.CreateInstance(typeof(TDialogSet), dialogStatePropertyAccessor) as TDialogSet;
+
+            _rootDialogId = rootDialogId;
+        }
+
+        public DialogBot(TDialogSet dialogSet) : this(dialogSet, DefaultRootDialogId)
+        {
+        }
+
+        public DialogBot(TDialogSet dialogSet, string rootDialogId)
+        {
+            _dialogSet = dialogSet ?? throw new ArgumentException(nameof(dialogSet));
+
+            if (string.IsNullOrEmpty(rootDialogId))
+            {
+                throw new ArgumentNullException(nameof(rootDialogId));
+            }
+
+            _rootDialogId = rootDialogId;
+        }
+
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var dialogContext = await _dialogSet.CreateContextAsync(turnContext, cancellationToken).ConfigureAwait(false);
+            var dialogTurnResult = default(DialogTurnResult);
+
+            if (dialogContext.ActiveDialog != null)
+            {
+                dialogTurnResult = await dialogContext.ContinueDialogAsync(cancellationToken).ConfigureAwait(false);
+            }
+            else
+            {
+                dialogTurnResult = await dialogContext.BeginDialogAsync(_rootDialogId, cancellationToken: cancellationToken).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ libraries/integration/Dialogs.AspNet.Core/ServiceCollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Bot.Builder.Integration.AspNet.Core
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDialogBot<TDialogSet>(this IServiceCollection services, IStatePropertyAccessor<DialogState> dialogStatePropertyAccessor, Action<BotFrameworkOptions> configureAction = null)
+            where TDialogSet : DialogSet
+        {
+            if (services == null)
+            {
+                throw new ArgumentNullException(nameof(services));
+            }
+
+            services.AddBot(new DialogBot<TDialogSet>(dialogStatePropertyAccessor), configureAction);
+
+            return services;
+        }
+    }
+}
